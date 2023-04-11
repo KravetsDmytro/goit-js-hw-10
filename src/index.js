101a51e003bee7b1573fd7cb71a87d8d83f2ff9d
Kravets_Dmytro <ditur@ukr.net>
2023-04-11 22:30:47 +0300

    import { fetchCountries }

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`').then(response => {
+console.log(response.json)
+
+return response.json()})
+.then(countr=>{console.log(countr)})
+.catch(error=>{console.error()})
