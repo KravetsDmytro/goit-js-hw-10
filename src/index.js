07e4aaf0b1c772d3f497250b60e5fded32f5824d
Kravets_Dmytro <ditur@ukr.net>
2023-04-12 10:20:16 +0300

    перемінні + імпорт бібліотек

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`').then(response => {
+const searhBox=document.querySelector('input#search-box');
+const countryList=document.querySelector('.country-list');
+const countryInfo=document.querySelector('.country-info')
+
+
+fetch('`https://restcountries.com/v3.1/name/ukraine?fields=name,capital,population,flags,languages`').then(response => {
 console.log(response.json)
 
 return response.json()})
