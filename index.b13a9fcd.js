cd940da94efcd4b08421c97b4acb63f41fb197bf
KravetsDmytro <117300190+KravetsDmytro@users.noreply.github.com>
2023-04-12 07:20:45 +0000

    Deploying to gh-pages from @ KravetsDmytro/goit-js-hw-10@07e4aaf0b1c772d3f497250b60e5fded32f5824d 🚀

--- index.b13a9fcd.js
+++
@@ -1,2 +0,0 @@
-fetch("`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`").then((o=>(console.log(o.json),o.json()))).then((o=>{console.log(o)})).catch((o=>{console.error()}));
-//# sourceMappingURL=index.b13a9fcd.js.map
